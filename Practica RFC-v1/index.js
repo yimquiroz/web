c7418276f45111b3a103de918051461e060cb1f8
Alonso Yanez <60414201+Jaay98@users.noreply.github.com>
2020-11-22 16:18:04 -0800

    curp v1

---
+++ Practica RFC-v1/index.js
@@ -0,0 +1,27 @@
+const express= require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const expressLayouts = require ('express-ejs-layouts')
+const port = process.env.PORT || 8080;
+
+//use ejs and express layouts
+
+app.set('view engine','ejs');
+app.use(expressLayouts);
+
+app.use(express.static('public'));
+//use body parser
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+//route our app
+
+const router = require('./app/routes');
+app.use('/',router);
+
+//start the server
+
+app.listen(port, function()
+{
+    console.log('app started in port 8080');
+});
