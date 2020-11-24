ebbdab1c2e4024903af264f222f793b4fffb72fd
Alonso Yanez <60414201+Jaay98@users.noreply.github.com>
2020-11-23 21:04:16 -0800

    Update routes.js

--- Examen_Unidad_3/app/routes.js
+++ Examen_Unidad_3/app/routes.js
@@ -13,10 +13,19 @@ router.get('/', (req, res)=>{
     res.render('pages/home',{extra:"Practica RFC"});
 });
 
-//route about
+//route Ventas
 router.post('/contact',(req,res)=>{
 
   var Nombre=req.body.Name.substr(0,1);
+  Nombre;
+   const personal={message:' Su CURP a sido generado '+ CURP};
+   res.render('pages/thank-you',{per: personal});
+});
+
+//route Compras
+router.post('/contact',(req,res)=>{
+
+ // var Nombre=req.body.Name.substr(0,1);
   
    const personal={message:' Su CURP a sido generado '+ CURP};
    res.render('pages/thank-you',{per: personal});
