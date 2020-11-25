4a6c68d47a11f1605a4e6482a4fed3bb48baaec5
Alonso Yanez <60414201+Jaay98@users.noreply.github.com>
2020-11-24 18:31:20 -0800

    ventas compras
    se sube modulo funcional, pdte de detalles

--- Examen_Unidad_3/app/routes.js
+++ Examen_Unidad_3/app/routes.js
@@ -15,15 +15,34 @@ router.get('/', (req, res)=>{
 
 //route Ventas
 
+router.get('/venta',(req,res)=>{
+   res.render('pages/venta');
+});
+
 router.post('/venta',(req,res)=>{
 
-  var Nombre=req.body.Name.substr(0,1);
-  Nombre;
-   const personal={message:' Su CURP a sido generado '+ CURP};
-   res.render('pages/thank-you',{per: personal});
+   var dlls=1000;
+   var pesos=20000;
+   var compra=19.20;
+
+   var cambio = req.body.cantidad; 
+
+   
+      if(cambio >= dlls){
+         result="No hay dinero suficiente";
+      }
+      else{
+        result = cambio * compra;
+        //console.log("Su cambio es: " + result);
+      }
 });
 
 //route Compras
+
+router.get('/compra',(req,res)=>{
+   res.render('pages/compra');
+});
+
 router.post('/compra',(req,res)=>{
     
    var dlls=1000;
@@ -34,11 +53,11 @@ router.post('/compra',(req,res)=>{
 
    
       if(cambio >= dlls){
-        console.log("No hay dinero suficiente");
+        result="No hay dinero suficiente";
       }
       else{
         result = cambio * compra;
-        //console.log("Su cambio es: " + result);
+        
       }
     
     
@@ -49,9 +68,4 @@ router.post('/compra',(req,res)=>{
 
 
 
- //route thank you
 
-
-router.get('/thank-you',(req,res)=>{
-   res.render('pages/compra');
-});
